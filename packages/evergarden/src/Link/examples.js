7271e7b90fde43e1a718eb4c78aba20bcf36e8de
EGOIST <0x142857@gmail.com>
2019-09-25 00:03:23 +0800

    Always selling

--- packages/evergarden/src/Link/examples.js
+++ packages/evergarden/src/Link/examples.js
@@ -6,8 +6,8 @@ export const basic = () => ({
   render(h) {
     return (
       <Stack>
-        <Link >Ever Link</Link>
-        <Link href="https://evergarden-ui.com">Ever Link to evergarden UI</Link>
+        <Link>Ever Link</Link>
+        <Link href="https://patreon.com/egoist">Support</Link>
         <Link isDisabled>disabled Ever Link</Link>
         <Link isExternal>external Ever Link. Would open in new tab</Link>
       </Stack>
