c589271828c63fc6442cea2ddd9cc0e469ab698f
Markus Siering <markus277@gmail.com>
2019-10-10 18:30:41 +0200

    chore: add knobs to button component (#2)
    * chore: add storybook knobs
    
    * feat: use knobs in button component story
    
    * use render function

--- packages/evergarden/src/Button/examples.js
+++ packages/evergarden/src/Button/examples.js
@@ -1,97 +1,55 @@
-import { Button, ButtonGroup } from 'evergarden'
+import Vue from 'vue'
+import { Button } from 'evergarden'
+import { addDecorator } from '@storybook/vue'
 import { action } from '@storybook/addon-actions'
+import { withKnobs, text, select, boolean } from '@storybook/addon-knobs'
+
+addDecorator(withKnobs)
+
+const variantColors = {
+  label: 'Variants',
+  options: ['green', 'red', 'cyan', 'orange', 'blue'],
+  defaultValue: 'green'
+}
+
+const sizes = {
+  label: 'Sizes',
+  options: ['xs', 'sm', 'md', 'lg'],
+  defaultValue: 'md'
+}
 
 export default { title: 'Button' }
 
-export const colors = () => ({
-  render() {
-    return (
-      <ButtonGroup>
-        <Button variantColor="green">Button</Button>
+export const basic = () => ({
+  props: {
+    variantColor: {
+      default: select(
+        variantColors.label,
+        variantColors.options,
+        variantColors.defaultValue
+      )
+    },
+    size: {
+      default: select(sizes.label, sizes.options, sizes.defaultValue)
+    },
+    text: {
+      default: text('Button text', 'Click me!')
+    },
+    disabled: {
+      default: boolean('Disabled', false)
+    }
+  },
 
-        <Button variantColor="red">Button</Button>
-
-        <Button variantColor="cyan">Button</Button>
-
-        <Button variantColor="orange">Button</Button>
-
-        <Button
-          variantColor="blue"
-          color="gray.300"
-          borderRadius="lg"
-          _hover={{ color: 'gray.700' }}
-        >
-          Compose
-        </Button>
-      </ButtonGroup>
-    )
-  }
-})
-
-export const variants = () => ({
   render(h) {
     return (
-      <ButtonGroup>
-        <Button variantColor="green" variant="solid">
-          Button
-        </Button>
-
-        <Button variantColor="green" variant="outline">
-          Button
-        </Button>
-
-        <Button variantColor="green" variant="ghost">
-          Button
-        </Button>
-
-        <Button variantColor="green" variant="link">
-          Button
-        </Button>
-      </ButtonGroup>
-    )
-  }
-})
-
-export const sizes = () => ({
-  render(h) {
-    return (
-      <ButtonGroup>
-        <Button variantColor="green" size="xs">
-          Button
-        </Button>
-
-        <Button variantColor="green" size="sm">
-          Button
-        </Button>
-
-        <Button variantColor="green" size="md">
-          Button
-        </Button>
-
-        <Button variantColor="green" size="lg">
-          Button
-        </Button>
-      </ButtonGroup>
-    )
-  }
-})
-
-export const events = () => ({
-  render(h) {
-    return (
-      <ButtonGroup>
-        <Button onClick={action('hello')}>Click Me!</Button>
-      </ButtonGroup>
-    )
-  }
-})
-
-export const states = () => ({
-  render(h) {
-    return (
-      <ButtonGroup>
-        <Button isDisabled={true}>Disabled</Button>
-      </ButtonGroup>
+      <Button
+        variantColor={this.variantColor}
+        size={this.size}
+        isDisabled={this.disabled}
+        onClick={action('hello')}
+      >
+        {this.text}
+      </Button>
     )
   }
 })
